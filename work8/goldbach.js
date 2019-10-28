f263fbd0574e6de8684e93a73dd66de4da5180c0
rainy-night-scholar <2079155186@qq.com>
2019-10-28 09:01:04 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -20,7 +20,7 @@ function bach(i){
      for(var x=2;x<odd;x++){
         var y =odd-x;
         if(bach(x)&&bach(y)&&x<=y){
-       str +=(odd+"可以拆分为两个质数" +x +"与" +y +"的和"+ '<br>')
+       str +=(odd+"可以拆分为两个质数" +  x +"与" + y +"的和"+ '<br>')
         }
     }
     document.getElementById('goldbach').innerHTML = str;
